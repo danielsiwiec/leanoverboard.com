a2d262dbfd48ecde8c3bb85dad0ee72d06680e50
Dan Siwiec <daniel.siwiec@gmail.com>
2015-04-08 00:45:53 -0700

    Add post stats

---
+++ _includes/postStats.js
@@ -0,0 +1,62 @@
+// Load the Visualization API and the piechart package.
+google.load('visualization', '1', {
+  'packages': ['corechart']
+});
+
+// Set a callback to run when the Google Visualization API is loaded.
+google.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  // Create our data table out of JSON data loaded from server.
+  var data = new google.visualization.DataTable();
+  data.addColumn('date', 'Date');
+  data.addColumn('number', 'Posts');
+
+  {% for post in site.posts %}
+  data.addRow([new Date({{ post.date | date: '%s' }} * 1000), 1]);
+  {% endfor %}
+
+  function floorDate(date) {
+    var newDate = new Date(date);
+    newDate.setDate(1);
+    return newDate;
+  }
+
+  var aggregate = google.visualization.data.group(data, [{
+    column: 0,
+    modifier: floorDate,
+    type: 'date'
+  }], [{
+    column: 1,
+    aggregation: google.visualization.data.sum,
+    type: 'number'
+  }]);
+
+  // Instantiate and draw our chart, passing in some options.
+  var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+
+  var options = {
+    title: 'Posts/time',
+    legend: {
+      position: 'none'
+    },
+    hAxis: {
+      gridlines: {
+        color: 'transparent'
+      }
+    },
+    vAxis: {
+      gridlines: {
+        color: 'transparent'
+      }
+    }
+  };
+
+  chart.draw(aggregate, options);
+}
+
+$(document).ready(function() {
+  $(window).resize(function() {
+    drawChart();
+  });
+});
---
+++ stats.md
@@ -0,0 +1,10 @@
+---
+layout: page
+permalink: /stats/
+---
+
+<script type="text/javascript">
+  {% include postStats.js %}
+</script>
+
+<div id="chart_div" style="width:100%; height:300px"></div>
